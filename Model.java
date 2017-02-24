4091ccfcd5f513078f2b950d3dac8ae88192fc93
Josh <jsdoman97@gmail.com>
2017-02-23 23:01:35 -0500

    Added movie times for single movie at single date

--- Model.java
+++ Model.java
@@ -12,7 +12,7 @@ public class Model {
 		try {				
 			FileMovieMaker fmm = FileMovieMaker.make(file);
 			movies = fmm.getMovies();
-			getTimes(movies.getFirst(), 1379);
+			printTimesForMovie(movies.getFirst());
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (FileMovieMaker.FormatException e) {
@@ -20,19 +20,48 @@ public class Model {
 		}	
 	}
 	
-	public LinkedList<String> getTimes(Movie movie, int endTime) {
+	public void printTimesForMovie(Movie movie) {
+		String line = movie.getTitle() + " - " + "Rated " + movie.getRating() +", " + Model.getProperTime(movie.getMinutes());
+		System.out.println(line);
+		LinkedList<String> times = getTimes(movie, 480, 1379);
+		for (String time : times) {
+			System.out.println(time);
+		}
+	}
+	
+	public LinkedList<String> getTimes(Movie movie, int open, int endTime) {
+		LinkedList<String> times = new LinkedList<String>();
+		
 		int duration = movie.getMinutes();
 		int startTime = endTime - duration;
 		
-		String line = "  " + Model.getProperTime(startTime) + " - " + Model.getProperTime(endTime);
-		System.out.println(line);
-		return null;
+		while(startTime > open + 60) {
+			String line = "  " + Model.getProperTime(startTime) + " - " + Model.getProperTime(endTime);
+			times.addFirst(line);
+			
+			endTime = startTime - 36;
+			startTime = endTime - duration;
+		}
+		
+		return times;
 	}
 		
 	public static String getProperTime(int minutes) {
 		int hours = minutes / 60;
 		int min = minutes % 60;
-		return Integer.toString(hours) + ":" + Integer.toString(min);
+		
+		String strHr = Integer.toString(hours);
+		String strMin = Integer.toString(min);
+		
+		if (min < 10) {
+			strMin = "0" + strMin;
+		}
+		
+//		if (hours < 10) {
+//			strHr = "0" + strHr;
+//		}
+		
+		return strHr + ":" + strMin;
 	}
 	
 	
